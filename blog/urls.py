ef34fd75080831cf57d3874f46b4dc43cc62243c
chjs <ch.jungsik@gmail.com>
2024-12-22 09:23:17 +0900

    resolved minor issues

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view()),
-    path('category/<str:slug>/', views.category_page),
+    path('', views.PostList.as_view(), name='blog'),
+    path('<int:pk>/', views.PostDetail.as_view(), name='blog'),
+    path('category/<str:slug>/', views.category_page, name='blog'),
 ]
